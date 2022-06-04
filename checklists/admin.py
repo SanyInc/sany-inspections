d3defd3d371c984d51d3e6e775f0bdfa8b7e1454
SanyInc <sany.inspections@gmail.com>
2022-06-04 15:21:15 +0300

    checklist admin slug change

--- checklists/admin.py
+++ checklists/admin.py
@@ -5,22 +5,22 @@ from .models import Activity, TypeOfActivity, Category, Choice, Question
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ("title", "order", )
     list_filter = ("title", )
-    prepopulated_fields = {"slug": ("title", )}
+    # prepopulated_fields = {"slug": ("title", )}
 
 class TypeOfActivityAdmin(admin.ModelAdmin):
     list_display = ("title", "activity", )
     list_filter = ("title", "activity",)
-    prepopulated_fields = {"slug": ("title", )}
+    # prepopulated_fields = {"slug": ("title", )}
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("title", )
     list_filter = ("activity__title", )
-    prepopulated_fields = {"slug": ("title", )}
+    # prepopulated_fields = {"slug": ("title", )}
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "get_choices","is_important", )
     list_filter = ("category__activity__title",)
-    prepopulated_fields = {"slug": ("title", )}
+    # prepopulated_fields = {"slug": ("title", )}
 
 admin.site.register(Activity, ActivityAdmin)
 admin.site.register(TypeOfActivity, TypeOfActivityAdmin)
